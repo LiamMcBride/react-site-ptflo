9bcfae4025123488e613d54cfa6e1fe5c3ade3f8
LiamMcBride <mailmcbride56@gmail.com>
2024-07-23 13:26:35 -0400

    Added pop up window for project full page.

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,11 +1,23 @@
 import './Project.css'
-import { Header5 } from './Text';
+import { Header2, Header5 } from './Text';
 import {useState} from 'react'
 import CategoryTag from './Tag';
 
+export function ProjectFull({pj, onClose}) {
+    return (
+        <div className="project-full-background">
+            <div className="project-full">
+                <Header2>{pj.title}</Header2>
+                <img alt="change" src={process.env.PUBLIC_URL + '/Assets/' + pj.image} />
+                <button onClick={onClose}>close</button>
+            </div>
+        </div>
+    )
+}
+
 
 //TODO: fix alt text
-function Project({pj}) {
+function Project({pj, onProjectClick}) {
     const [hovered, setHovered] = useState(false)
 
     const HoverScreen = () => {
@@ -18,7 +30,7 @@ function Project({pj}) {
     }
 
     return (
-        <button className="project" onMouseOver={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+        <button onClick={() => {onProjectClick(); setHovered(false)}} className="project" onMouseOver={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
             <Header5>{pj.title}</Header5>
             <br></br>
             <img alt="change" src={process.env.PUBLIC_URL + '/Assets/' + pj.image} />
--- src/Components/Slices/ProjectsPage.js
+++ src/Components/Slices/ProjectsPage.js
@@ -1,14 +1,19 @@
 import './ProjectsPage.css';
 import {Header2} from '../Text'
-import Project from '../Project';
+import Project, { ProjectFull } from '../Project';
 import { projects_data } from '../../Data/ProjectData';
+import {useState} from 'react'
 
 function ProjectsPage({children}) {
+
+    const [currentProject, setCurrentProject] = useState(null)
+
   return (
     <section id="projects-page" className="projects-page">
         <Header2>Projects</Header2>
+        {currentProject != null ? <ProjectFull onClose={() => setCurrentProject(null)} pj={currentProject}/> : null}
         <div className="project-grid">
-            {projects_data.map((pj, i) => <Project key={i} pj={pj}/>)}
+            {projects_data.map((pj, i) => <Project onProjectClick={() => setCurrentProject(pj)} key={i} pj={pj}/>)}
         </div>
     </section>
   );
